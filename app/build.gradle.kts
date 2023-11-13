c048ede7b0fbef9e803f1a8273fed6374d3e38cb
Khalid Radwan <31837258+khalidr12@users.noreply.github.com>
2023-11-13 01:01:04 -0500

    edit view for podcast item with image and title

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+    implementation ("io.coil-kt:coil-compose:1.4.0")
+
 
 }
 
--- app/src/main/java/com/audiobooks/codingchallenge/view/PodcastItemView.kt
+++ app/src/main/java/com/audiobooks/codingchallenge/view/PodcastItemView.kt
@@ -1,14 +1,37 @@
 package com.audiobooks.codingchallenge.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.audiobooks.codingchallenge.api.response.Podcast
 
 @Composable
 fun PodcastItem(podcast: Podcast){
-    Text(
-        text = "Hello ${podcast.title}!",
-        modifier = Modifier
-    )
+    Row {
+        val painter: Painter = rememberImagePainter(podcast.image)
+        Image(
+            painter = painter,
+            contentDescription = null, // Add content description if needed
+            modifier = Modifier
+                .size(100.dp, 100.dp) // Adjust size as needed
+                .clip(RoundedCornerShape(10.dp))
+        )
+        Column {
+            Text(
+                text = "Hello ${podcast.title}!",
+                modifier = Modifier
+            )
+        }
+    }
 }
