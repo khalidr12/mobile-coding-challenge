805e2da557926c9f23973f79d92a130f321c63f4
Khalid Radwan <31837258+khalidr12@users.noreply.github.com>
2023-11-17 03:35:02 -0500

    fix loading shimmer:

--- app/src/main/java/com/audiobooks/codingchallenge/view/LoadingView.kt
+++ app/src/main/java/com/audiobooks/codingchallenge/view/LoadingView.kt
@@ -1,12 +1,20 @@
 package com.audiobooks.codingchallenge.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.valentinilk.shimmer.shimmer
 
@@ -20,12 +28,32 @@ fun LoadingCellView(){
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(vertical = 10.dp)
             .shimmer()
     ) {
         Box(
             modifier = Modifier
-                .size(100.dp)
-                .padding(10.dp)
+                .size(65.dp, 65.dp)
+                .clip(RoundedCornerShape(5.dp))
+                .background(Color.LightGray)
         )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Box(
+                modifier = Modifier
+                    .height(18.dp)
+                    .width(180.dp)
+                    .background(Color.LightGray)
+                    .clip(RoundedCornerShape(5.dp))
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Box(
+                modifier = Modifier
+                    .height(10.dp)
+                    .width(120.dp)
+                    .background(Color.LightGray)
+                    .clip(RoundedCornerShape(5.dp))
+            )
+        }
     }
 }
