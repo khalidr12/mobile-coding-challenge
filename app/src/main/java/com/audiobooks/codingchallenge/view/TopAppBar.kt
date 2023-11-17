fed77f90a45ed7d278c9ed271d49e091c42fbb10
Khalid Radwan <31837258+khalidr12@users.noreply.github.com>
2023-11-17 04:44:07 -0500

    fix icon for the top nav bar

--- app/src/main/java/com/audiobooks/codingchallenge/view/TopAppBar.kt
+++ app/src/main/java/com/audiobooks/codingchallenge/view/TopAppBar.kt
@@ -1,20 +1,21 @@
 package com.audiobooks.codingchallenge.view
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.audiobooks.codingchallenge.R
 import com.audiobooks.codingchallenge.viewmodel.PodcastViewModel
 
 @Composable
@@ -28,11 +29,11 @@ fun TopAppBar(viewModel: PodcastViewModel){
             },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_arrow_back),
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowLeft,
             contentDescription = null,
-            modifier = Modifier
-                .size(20.dp, 20.dp)
+            tint = Color.Red,
+            modifier = Modifier.size(20.dp)
         )
         Text(
             text = "Back",
